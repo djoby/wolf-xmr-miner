f9b2b275d9a0207bff82fdac47a63ada98cb70bf
djoby <djoby@mockmyid.com>
2017-06-25 21:14:41 +0000

    finished cleaning unused code

--- minerutils.c
+++ minerutils.c
@@ -38,76 +38,6 @@ int ASCIIHexToBinary(void *restrict rawstr, const char *restrict asciistr, size_
 	return(len >> 1);
 }
 
-uint32_t BSWAP32(uint32_t data)
-{
-	#ifdef __GNUC__
-	return(__builtin_bswap32(data));
-	#else
-	#error "Unsupported compiler."
-	#endif
-}
-
-// Parameter chunks is the number of 32-bit chunks in the buffer
-// Buffer length MUST be divisible by 4 bytes, or last chunk
-// should be omitted (for our uses, this should never happen)
-void SwapBuffer32(void *data, int chunks)
-{
-	for(int i = 0; i < chunks; ++i) ((uint32_t *)data)[i] = BSWAP32(((uint32_t *)data)[i]);
-}
-
-// Pointer returned in Output must be freed
-size_t LoadTextFile(char **Output, char *Filename)
-{
-	size_t len;
-	FILE *kernel = fopen(Filename, "rb");
-	
-	fseek(kernel, 0, SEEK_END);
-	len = ftell(kernel);
-	fseek(kernel, 0, SEEK_SET);
-	
-	*Output = (char *)malloc(sizeof(char) * (len + 2));
-	len = fread(*Output, sizeof(char), len, kernel);
-	Output[0][len] = 0x00;		// NULL terminator
-	fclose(kernel);
-	
-	return(len);
-}
-
-bool FullTest(const uint32_t *Hash, const uint32_t *FullTarget)
-{
-	bool ret;
-	
-	for(int i = 7; i >= 0; --i)
-	{
-		ret = Hash[i] <= FullTarget[i];
-		if(Hash[i] != FullTarget[i]) break;
-	}
-	
-	return(ret);
-}
-
-void CreateTargetFromDiff(uint32_t *FullTarget, double Diff)
-{
-	int i;
-	uint64_t tmp;
-	
-	for(i = 6; i > 0 && Diff > 1.0; --i) Diff /= 4294967296.0;
-	
-	tmp = 4294901760.0 / Diff;
-	
-	if(!tmp && i == 6)
-	{
-		memset(FullTarget, 0xFF, 32);
-	}
-	else
-	{
-		memset(FullTarget, 0x00, 32);
-		
-		FullTarget[i] = (uint32_t)tmp;
-		FullTarget[i + 1] = (uint32_t)(tmp >> 32);
-	}
-}
-
 #ifdef __linux__
 
 TIME_TYPE MinerGetCurTime(void)
@@ -137,14 +67,3 @@ double SecondsElapsed(TIME_TYPE Start, TIME_TYPE End)
 
 #endif
 
-#ifdef __linux__
-
-void Sleep(uint32_t ms)
-{
-	struct timespec t;
-	t.tv_sec = ms / 1000;
-	t.tv_nsec = (ms % 1000) * 1000000;
-	nanosleep(&t, NULL);
-}
-
-#endif
